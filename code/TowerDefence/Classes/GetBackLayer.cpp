b97a62cec09d66738e7d3d7dc75a01ac38058eb8
ybdesire <ybdesire@gmail.com>
2007-08-23 01:18:05 +0800

    Add get back function for GetBackLayer class.

--- code/TowerDefence/Classes/GetBackLayer.cpp
+++ code/TowerDefence/Classes/GetBackLayer.cpp
@@ -1,4 +1,5 @@
 #include "GetBackLayer.h"
+#include "MainScene.h"
 
 GetBackLayer::GetBackLayer()
 {
@@ -10,5 +11,26 @@ GetBackLayer::~GetBackLayer()
 
 bool GetBackLayer::init()
 {
+	auto visibleSize = Director::getInstance()->getVisibleSize();
+	
+	auto btnTitle = Label::create("Back","Arial",30);
+	auto norBtnSprite = Scale9Sprite::create("Button/public_ui_blue_btn.png");
+	auto highlightBtnSprite = Scale9Sprite::create("Button/public_ui_green_btn.png");
+
+	auto getBackBtn = ControlButton::create(norBtnSprite, highlightBtnSprite);
+	getBackBtn->setBackgroundSpriteForState(highlightBtnSprite, Control::State::HIGH_LIGHTED);
+	getBackBtn->setPosition(visibleSize.width - norBtnSprite->getContentSize().width/2, visibleSize.height/2 - norBtnSprite->getContentSize().height);
+
+	getBackBtn->addTargetWithActionForControlEvents(this, cccontrol_selector(GetBackLayer::getBack), Control::EventType::TOUCH_UP_INSIDE);
+
+	this->addChild(getBackBtn, 20);
+
 	return true;
+}
+
+
+void GetBackLayer::getBack(Ref *pSender, Control::EventType event)
+{
+	auto scene = MainScene::createScene();
+	Director::getInstance()->replaceScene(scene);
 }
