abe40a2c99b1bb4bc5d42141db65eb12765187d3
ybdesire <ybdesire@gmail.com>
2014-11-14 18:14:12 +0800

    complete createLvlLabel() function, which can create a level label.

--- code/TowerDefence/Classes/lvlChooseScene.cpp
+++ code/TowerDefence/Classes/lvlChooseScene.cpp
@@ -25,4 +25,26 @@ bool lvlChooseScene::init()//create() function will call init()
 	return true;
 }
 
+Label* lvlChooseScene::createLvlLabel(const char *lvlstr)
+{
+	auto lvlLable = Label::create(lvlstr, "Arail", 60);
+	lvlLable->setColor(Color3B::RED);
+	lvlLable->setPosition(ccp(60, 60));//Button size is 130x130
+	return lvlLable;
+}
 
+
+void lvlChooseScene::lvl1(Ref *pSender)
+{
+	CCLOG("level 1");
+}
+
+void lvlChooseScene::lvl2(Ref *pSender)
+{
+	CCLOG("level 2");
+}
+
+void lvlChooseScene::lvl3(Ref *pSender)
+{
+	CCLOG("level 3");
+}
