da47de13c1850301697da6ca97f6d2dad7cac62d
ybdesire <ybdesire@gmail.com>
2014-11-17 23:20:01 +0800

    Add getBack() function to the class

--- code/TowerDefence/Classes/GetBackLayer.h
+++ code/TowerDefence/Classes/GetBackLayer.h
@@ -2,9 +2,10 @@
 #define _GETBACKLAYER_H_
 
 #include "cocos2d.h"
-
+#include "cocos-ext.h"
 
 using namespace cocos2d;
+using namespace cocos2d::extension;
 
 class GetBackLayer: public Layer
 {
@@ -13,6 +14,9 @@ public:
 	~GetBackLayer();
 	CREATE_FUNC(GetBackLayer);
 	virtual bool init(); 
+
+private:
+	void getBack(Ref *pSender, Control::EventType event);
 };
 
 #endif
