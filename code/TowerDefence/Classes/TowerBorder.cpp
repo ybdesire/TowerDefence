0a9cdccd9ad7091719abfe6979c0696a43f40627
ybdesire <ybdesire@gmail.com>
2014-12-23 08:52:25 +0800

    Add isClickMe and TowerBorder sprite init functions.

--- code/TowerDefence/Classes/TowerBorder.cpp
+++ code/TowerDefence/Classes/TowerBorder.cpp
@@ -1,9 +1,5 @@
 #include "TowerBorder.h"
 
-bool TowerBorder::init()
-{
-	return true;
-}
 
 Hero *TowerBorder::getHero()
 {
@@ -13,4 +9,40 @@ Hero *TowerBorder::getHero()
 void TowerBorder::bindHero(Hero *phero)
 {
 	_hero = phero;
+}
+
+bool TowerBorder::init()
+{
+	__String *sFilePath = __String::createWithFormat("sprite/hero/border_%d.png", _level);
+	Sprite *sprite = Sprite::create(sFilePath->getCString());
+
+	bindSprite(sprite);
+
+
+	ActionInterval *rotateBy = RotateBy::create(25.0f, 360, 360);
+	auto repeat = RepeatForever::create(rotateBy);
+
+	sFilePath = __String::createWithFormat("sprite/hero/magic_border_%d.png", _level);
+	sprite = Sprite::create(sFilePath->getCString());
+	sprite->setOpacity(80);
+	sprite->runAction(repeat);
+
+	this->addChild(sprite);
+	return true;
+}
+
+bool TowerBorder::isClickMe(Point pos)
+{
+	Size size = getSprite()->getContentSize();
+	Point borderPos = getPosition();
+
+	Point srcPos = Point(borderPos.x-size.width, borderPos.y+size.height);
+	Point detPos = Point(borderPos.x+size.width, borderPos.y-size.height);
+
+	if(pos.x>=srcPos.x && pos.y>=srcPos.y && pos.x<=detPos.x && pos.y<=detPos.y)
+	{
+		return true;
+	}
+
+	return false;
 }
